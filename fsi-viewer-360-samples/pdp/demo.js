c86563b0b9ca4e6057f1c85d0703f9e8f1465b4f
neptunelabs <webmaster@neptunelabs.com>
2021-08-17 09:34:09 +0000

    JSFiddle commit

--- fsi-viewer-360-samples/pdp/demo.js
+++ fsi-viewer-360-samples/pdp/demo.js
@@ -1,20 +1,15 @@
-function changeImage(buttonID) {
-  let img;
-  let curImage = document.getElementById('image');
+function changeImage(buttonID){
+  let dir;
   switch (buttonID) {
     case "0":
-      img =  '//fsi-site.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
+      dir =  'images/samples/Shoe/View1';
       break
     case "1":
-      img = '//fsi-site.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg&width=940&rect=0,0.29634,1,0.66709';
+      dir = 'images/samples/Shoe/View2';
       break
-    case "2":
-      img = '//fsi-site.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg&width=940';
-      break
-    default:
-      img = '//fsi-site.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
   }
-  curImage.src = img;
+  document.getElementsByTagName("fsi-viewer")[0].changeConfig(undefined, {dir: dir});
+
 }
 
 
